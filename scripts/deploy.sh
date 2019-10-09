d03311b7242f0633c55f988c968b346b1786682c
Petr Moravsky <pmoravs@amazon.com>
2019-10-09 00:54:22 -0700

    Update Deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,9 +4,8 @@
 # install node
 pushd .
 cd ~
-curl -sL https://deb.nodesource.com/setup_6.x -o nodesource_setup.sh
-sudo bash nodesource_setup.sh
-sudo apt-get install nodejs -y
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+sudo apt-get install -y nodejs
 sudo apt-get install build-essential -y
 
 # install express
